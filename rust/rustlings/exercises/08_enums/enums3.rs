ab61f2d413d70c445b8b9d9c966a2e91cbc5aa1c
Pratyush kiran Rout <pratyush@email.com>
2026-07-04 19:19:34 +0530

    feat:rustling practice

--- rust/rustlings/exercises/08_enums/enums3.rs
+++ rust/rustlings/exercises/08_enums/enums3.rs
@@ -46,6 +46,23 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => {
+                self.resize(width, height);
+            }
+            Message::ChangeColor(red, green, blue) => {
+                self.change_color(red, green, blue);
+            }
+            Message::Echo(state) => {
+                self.echo(state.clone());
+            }
+            Message::Move(state) => {
+                self.move_position(state);
+            }
+            Message::Quit => {
+                self.quit();
+            }
+        }
     }
 }
 
--- rust/structLearning/src/main.rs
+++ rust/structLearning/src/main.rs
@@ -2,6 +2,33 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+impl Point {
+    fn get_x(&mut self) -> &mut i32 {
+        &mut self.x
+    }
+}
 
 fn main() {
     let width1 = 30;
@@ -18,17 +45,26 @@ fn main() {
         width: 30,
         height: 20,
     };
+    let rectangle2 = Rectangle {
+        width: 10,
+        height: 10,
+    };
+    println!("Can rect1 hold rect2 {}", rectangle1.can_hold(&rectangle2));
     println!(
         "The area of the rectangle is {} square pixels using area3 function",
-        area3(&rectangle1)
+        rectangle1.area()
     );
+    if rectangle1.width() {
+        println!("The width of the rectangle is greater than 0");
+    }
+    let mut p = Point { x: 1, y: 2 };
+    let x = p.get_x();
+    *x += 1;
+    println!("{} {}", *x, p.y);
 }
 fn area(width: u32, height: u32) -> u32 {
     width * height
 }
 fn area2(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
-}
-fn area3(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
